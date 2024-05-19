5384d9c3e995a14d92debcbd7477d642d0189e37
Irishtheboy <francolukhele14@gmail.com>
2024-05-20 00:39:11 +0200

    12:38 5/20/2024
    we just need background image

--- src/librarysystemrun/Gui.java
+++ src/librarysystemrun/Gui.java
@@ -6,18 +6,27 @@ package librarysystemrun;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 /**
  *
  * @author Sabura11
  */
-public class Gui extends JFrame {
+public class Gui extends JFrame implements ActionListener {
 
     //these properties are for the login make sure to categorize the rest please
-    private JLabel lblUsername, lblPassword, toplbl, lblwest, lblsouth,blank1,blank2,blank3,blank4;
+    private JLabel lblUsername, lblPassword, toplbl, lblwest, lblsouth, blank1, blank2, blank3, blank4;
     private JTextField txtUsername, txtPassword;
     private JButton loginBtn, SignUpBtn;
     private JPanel northPnl, centerPnl, southPnl, eastPnl, westPnl;
+
+    //this is for the sign up page
+    private JLabel emaillbl, phonelbl;
+    private JTextField emailtxt, phonetxt;
+    private JButton spBtn, returnBtn;
+    
+    //top menu section
+    private JButton bookBtn, audioBtn, exitBtn,logoutBtn;
 
     public Gui() {
         super();
@@ -44,20 +53,41 @@ public class Gui extends JFrame {
         southPnl = new JPanel();
         eastPnl = new JPanel();
         westPnl = new JPanel();
-        
-        northPnl.setPreferredSize(new Dimension(500,90));
-        centerPnl.setPreferredSize(new Dimension(100,1));
-        eastPnl.setPreferredSize(new Dimension(100,400));
-        westPnl.setPreferredSize(new Dimension(100,400));
-        
+
+        northPnl.setPreferredSize(new Dimension(500, 90));
+        centerPnl.setPreferredSize(new Dimension(100, 1));
+        eastPnl.setPreferredSize(new Dimension(100, 400));
+        westPnl.setPreferredSize(new Dimension(100, 400));
+
         txtUsername.setMaximumSize(txtUsername.getPreferredSize());
         txtUsername.setMinimumSize(txtUsername.getPreferredSize());
         txtPassword.setMaximumSize(txtPassword.getPreferredSize());
         txtPassword.setMinimumSize(txtPassword.getPreferredSize());
-        
-        
+
+        loginBtn.addActionListener(this);
+        SignUpBtn.addActionListener(this);
 
         //this part should be for the sign up
+        emaillbl = new JLabel();
+        phonelbl = new JLabel();
+        emailtxt = new JTextField();
+        phonetxt = new JTextField();
+        spBtn = new JButton("Sign-Up!");
+        returnBtn = new JButton("Return to Login page");
+        spBtn.addActionListener(this);
+        returnBtn.addActionListener(this);
+
+        //topmenu
+        bookBtn = new JButton("Books");
+        audioBtn = new JButton("Audio");
+        exitBtn = new JButton("Exit");
+        logoutBtn = new JButton("Logout");
+        
+        bookBtn.addActionListener(this);
+        audioBtn.addActionListener(this);
+        exitBtn.addActionListener(this);
+        logoutBtn.addActionListener(this);
+        
     }
 
     // GUI for the Login people please dont forget
@@ -66,6 +96,12 @@ public class Gui extends JFrame {
         this.setVisible(true);
         this.setSize(500, 250);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        northPnl.removeAll();
+        centerPnl.removeAll();
+        southPnl.removeAll();
+        eastPnl.removeAll();
+        westPnl.removeAll();
 
         centerPnl.setLayout(new GridLayout(3, 2));
         southPnl.setLayout(new GridLayout(1, 2));
@@ -78,15 +114,15 @@ public class Gui extends JFrame {
         gbc.gridy = 0;
         gbc.insets = new Insets(10, 10, 10, 10);
         northPnl.add(toplbl);
-        
+
         centerPnl.add(lblUsername);
         centerPnl.add(txtUsername);
         centerPnl.add(lblPassword);
         centerPnl.add(txtPassword);
         centerPnl.add(blank1);
         centerPnl.add(blank2);
-//        centerPnl.add(blank3);
-//        centerPnl.add(blank4);
+//      centerPnl.add(blank3);
+//      centerPnl.add(blank4);
 
         southPnl.add(loginBtn);
         southPnl.add(SignUpBtn);
@@ -100,6 +136,119 @@ public class Gui extends JFrame {
         this.add(westPnl, BorderLayout.WEST);
         this.add(eastPnl, BorderLayout.EAST);
 
-//        this.pack();
+        this.revalidate();
+        this.repaint();
+
+    }
+
+    public void setSignUp() {
+        this.setTitle("Sign up");
+        this.setVisible(true);
+        this.setSize(500, 250);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        northPnl.removeAll();
+        centerPnl.removeAll();
+        southPnl.removeAll();
+        eastPnl.removeAll();
+        westPnl.removeAll();
+
+        centerPnl.setLayout(new GridLayout(5, 2));
+        southPnl.setLayout(new GridLayout(1, 2));
+        eastPnl.setLayout(new GridLayout(2, 1));
+        westPnl.setLayout(new GridLayout(2, 1));
+
+        northPnl.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        toplbl.setText("Sign Up");
+        northPnl.add(toplbl);
+
+        //
+        centerPnl.add(lblUsername);
+        centerPnl.add(txtUsername);
+        centerPnl.add(lblPassword);
+        centerPnl.add(txtPassword);
+        emaillbl.setText("Email");
+        centerPnl.add(emaillbl);
+        centerPnl.add(emailtxt);
+        phonelbl.setText("Phone");
+        centerPnl.add(phonelbl);
+        centerPnl.add(phonetxt);
+        centerPnl.add(blank1);
+        centerPnl.add(blank2);
+
+        southPnl.add(spBtn);
+        southPnl.add(returnBtn);
+
+        westPnl.add(lblwest);
+        eastPnl.add(lblsouth);
+        this.add(northPnl, BorderLayout.NORTH);
+        this.add(centerPnl, BorderLayout.CENTER);
+        this.add(southPnl, BorderLayout.SOUTH);
+        this.add(westPnl, BorderLayout.WEST);
+        this.add(eastPnl, BorderLayout.EAST);
+
+        this.revalidate();
+        this.repaint();
+    }
+
+    public void topMenu() {
+        this.setTitle("Sign up");
+        this.setVisible(true);
+        this.setSize(500, 250);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        northPnl.removeAll();
+        centerPnl.removeAll();
+        southPnl.removeAll();
+        eastPnl.removeAll();
+        westPnl.removeAll();
+
+         
+        centerPnl.setLayout(new GridLayout(2, 2));
+        southPnl.setLayout(new GridLayout(1, 2));
+
+        northPnl.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        toplbl.setText("Top Menu");
+        northPnl.add(toplbl);
+
+        centerPnl.add(bookBtn);
+        centerPnl.add(audioBtn);
+        centerPnl.add(blank1);
+        centerPnl.add(blank2);
+        southPnl.add(logoutBtn);
+        southPnl.add(exitBtn);
+        
+        this.add(centerPnl, BorderLayout.CENTER);
+        this.add(southPnl, BorderLayout.SOUTH);
+
+        this.revalidate();
+        this.repaint();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == loginBtn) {
+            JOptionPane.showMessageDialog(this, "Welcome");
+            this.topMenu();
+        } else if (e.getSource() == SignUpBtn) {
+            JOptionPane.showMessageDialog(this, "Welcome");
+            this.setSignUp();
+        } else if (e.getSource() == spBtn) {
+            JOptionPane.showMessageDialog(this, "You have signed up");
+        } else if (e.getSource() == returnBtn) {
+            this.SetLoginGui();
+        } else if (e.getSource() == exitBtn){
+            System.exit(0);
+        }
+
     }
 }
